2dda7430f3ed871983ccbfcdbea5a6b29293ba39
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-09-22 01:52:07 -0700

    set up components for first day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import About from "@/components/About";
+import Join from "@/components/Join";
+import Landing from "@/components/Landing";
+import What from "@/components/What";
+import { Joan } from "next/font/google";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+      <Landing />
+      <About />
+      <What />
+      <Join />
     </div>
   );
 };
