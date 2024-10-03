74ba1590967f34a13170dd368ecaf0fef8bb657a
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-10-03 16:50:08 -0700

    fix config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      sw: {
-        "gold-100": "#D09742",
-        "gold-200": "#6A4D22",
-        white: "#FFDA7C",
+    extend: {
+      colors: {
+        sw: {
+          "gold-100": "#D09742",
+          "gold-200": "#6A4D22",
+          white: "#FFDA7C",
+        },
       },
     },
   },
