4216754db1922a023860adda6da3cf07f970caed
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-10-03 12:35:36 -0700

    this is message

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Button from "./Button";
 
-
 const Landing = () => {
-
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-5">
-
-      <div className="flex justify-center items-center w-32 h-32 bg-sw-gold-100">
-block 1
+    <div className="flex w-full flex-col items-center justify-center gap-5">
+      <div className="flex h-32 w-32 items-center justify-center bg-sw-gold-100">
+        block 1
       </div>
-      <div className="w-32 h-32 bg-sw-gold-100">
-        block 2
-      </div>
-
-
+      <div className="h-32 w-32 bg-sw-gold-100">block 2</div>
 
       <Button title={"This is a title"} link={"/about"} />
     </div>
@@ -22,5 +15,3 @@ block 1
 };
 
 export default Landing;
-
-
