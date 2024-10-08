29f205c1997075a889c0918aab708411b5cb5ea0
DannielK <dannykm003@gmail.com>
2024-10-08 15:30:45 -0700

    Finished Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Inter, Lora } from "next/font/google";
 import { ReactQueryClientProvider } from "@/utils/react-query";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
+const lora = Lora({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Star Wars Website",
@@ -18,7 +19,7 @@ type LayoutProps = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${lora.className} ${inter.className}`}>
         <ReactQueryClientProvider>
           <Navigation />
           {children}
--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,1 +1,22 @@
-export const ITEMS = [];
+export const ITEMS = [
+  {
+    name: "ABOUT",
+    link: "/about",
+  },
+  {
+    name: "BOARD",
+    link: "/board",
+  },
+  {
+    name: "EVENTS",
+    link: "/events",
+  },
+  {
+    name: "GALLERY",
+    link: "/gallery",
+  },
+  {
+    name: "HISTORY",
+    link: "/history",
+  },
+];
