497d1e9b908ce7ecfa93f542c9c21fd693370960
sunbrandon <sunabrandon@gmail.com>
2024-10-14 12:20:31 -0700

    Fix

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,12 +6,12 @@ const Landing = () => {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="size-2/3 text-center">
-        <h1 className="text-transparent bg-gradient-to-b mb-4 from-[#DDA82A] via-sw-white to-[#905803] bg-clip-text text-4xl font-bold md:text-6xl">
+        <h1 className="mb-4 bg-gradient-to-b from-[#DDA82A] via-sw-white to-[#905803] bg-clip-text text-4xl font-bold text-transparent md:text-6xl">
           WELCOME TO
         </h1>
-          <div className="flex items-center justify-center">
-            <Image src={swLogo} alt="UCR SWC Logo" />
-          </div>
+        <div className="flex items-center justify-center">
+          <Image src={swLogo} alt="UCR SWC Logo" />
+        </div>
       </div>
     </div>
   );
