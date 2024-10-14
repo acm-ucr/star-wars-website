d85f4f9e39b7828ca1e4dc5951917552d1642f5d
sunbrandon <sunabrandon@gmail.com>
2024-10-14 12:19:57 -0700

    Added logo

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import Button from "./Button";
+import Image from "next/image";
+import swLogo from "@/public/swLOGO.svg";
 
 const Landing = () => {
   return (
-    <div className="flex flex-col">
-      Landing
-      <Button title={"This is a title"} link={"/about"} />
+    <div className="flex h-screen items-center justify-center">
+      <div className="size-2/3 text-center">
+        <h1 className="text-transparent bg-gradient-to-b mb-4 from-[#DDA82A] via-sw-white to-[#905803] bg-clip-text text-4xl font-bold md:text-6xl">
+          WELCOME TO
+        </h1>
+          <div className="flex items-center justify-center">
+            <Image src={swLogo} alt="UCR SWC Logo" />
+          </div>
+      </div>
     </div>
   );
 };
