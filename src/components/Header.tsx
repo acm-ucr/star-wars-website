7be2bd878c166ad9f77ea5b7c532fbcfcbdf9da9
Andrew Jang <jangandreww@gmail.com>
2024-10-13 17:23:41 -0700

    finished title comp 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,25 +3,26 @@ import Image from "next/image";
 import smallheaderframe from "@/public/frame/title2smallframe.svg";
 import starImage from "@/public/starframeIcon.svg";
 
-const Header = ({title}: {title: string}) => {
+const Header = ({ title }: { title: string }) => {
   return (
-    <div className = "relative">
-      <Image  src = {smallheaderframe}
-              alt = "smallheaderframe"
-              className = "object-cover rounded-xl bg-gradient-to-b from-[#15121A] via-[#2C2636] to-[#675980]"
+    <div className="relative">
+      <Image
+        src={smallheaderframe}
+        alt="smallheaderframe"
+        className="rounded-xl bg-gradient-to-b from-[#15121A] via-[#2C2636] to-[#675980] object-cover"
       />
 
-      <div className = "absolute top-0 left-1/2 transform translate-x-[-50%] translate-y-[-50%]">
-        <Image src={starImage} alt="Star"/>
+      <div className="absolute left-1/2 top-0 translate-x-[-50%] translate-y-[-50%] transform">
+        <Image src={starImage} alt="Star" />
       </div>
 
-      <div className="absolute text-center top-[28%] w-full">
-        <header className = "bg-gradient-to-b from-[#D09742] via-[#FFDA7C] to-[#6A4D22] text-transparent bg-clip-text font-nunito font-bold text-6xl">
+      <div className="absolute top-[28%] w-full text-center">
+        <header className="bg-gradient-to-b from-[#D09742] via-[#FFDA7C] to-[#6A4D22] bg-clip-text font-nunito text-6xl font-bold text-transparent">
           {title}
         </header>
       </div>
     </div>
-  )
+  );
 };
 
 export default Header;
