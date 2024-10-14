d9cfbbc5410ae61429a08fa04502491d3a3e1c2c
HaruSakamoto <harusakamoto803@gmail.com>
2024-10-14 01:28:09 -0700

    Finished Title Component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Title = () => {
-  return <div>Title</div>;
+  return ( 
+  <div className="p-10 bg-black">
+    <div className="text-5xl bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 text-transparent bg-clip-text leading-tight font-bold">
+    Forge the Future
+    </div>
+  </div>
+  );
 };
 
 export default Title;
