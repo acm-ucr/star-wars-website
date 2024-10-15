f18711307615549fff0beda8ca339dfae29ebd99
HaruSakamoto <harusakamoto803@gmail.com>
2024-10-15 00:25:51 -0700

    Modified Title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Title = () => {
+const Title = ({text}:{text:string}) => {
   return (
     <div className="bg-black p-10">
       <div className="bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text text-5xl font-bold leading-tight text-transparent">
-        Forge the Future
+        {text}
       </div>
     </div>
   );
