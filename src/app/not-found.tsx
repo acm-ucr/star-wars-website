a0099e3c88e0d431f48a63d0ca4737a8ccc98575
Howardwheeler <howard.wheeler159@gmail.com>
2024-10-14 22:07:42 -0700

    Finished 404 Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,11 +6,11 @@ const NotFound = () => {
   return (
     <div className="flex w-full">
       <div className="flex w-auto flex-col items-center">
-        <div className="flex flex-col items-center gap-y-5 px-[20%] py-[2.5%] font-lora font-semibold text-sw-gold-100 sm:text-3xl md:text-4xl lg:text-6xl">
-          <div>404</div>
-          <div className="flex h-[4px] w-1/5 rounded-lg bg-sw-gold-100" />
-          <div>PAGE NOT FOUND</div>
-          <p className="text-center font-normal text-white sm:text-sm md:text-base lg:text-2xl">
+        <div className="flex flex-col items-center gap-y-5 from-sw-gold-100 via-sw-white to-sw-gold-200 px-[22%] font-nunito font-bold text-transparent sm:text-3xl md:text-4xl lg:text-6xl">
+          <div className="bg-gradient-to-b bg-clip-text">404</div>
+          <div className="flex h-[5px] w-1/5 rounded-lg bg-gradient-to-r" />
+          <div className="bg-gradient-to-b bg-clip-text">PAGE NOT FOUND</div>
+          <p className="text-center font-normal text-white sm:text-sm md:text-base lg:text-3xl">
             It seems you've ventured into uncharted space. The page you're
             seeking is hidden in the outer rim or lost in hyperspace. But fear
             not, young Padawan! Use the Force (or the navigation menu) to find
@@ -18,7 +18,7 @@ const NotFound = () => {
             you on your journey!
           </p>
         </div>
-        <Link href="/" className="flex w-[30%] place-content-center">
+        <Link href="/" className="flex w-[35%] place-content-center pt-[3%]">
           <Image
             src={Home}
             alt="Back To Home"
