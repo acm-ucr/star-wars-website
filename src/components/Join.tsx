989239630f0155d354d0d790752452923e2360ce
Patrick Wu <89278486+exitcy@users.noreply.github.com>
2024-10-14 20:09:41 -0700

    Update Join.tsx
    tailwind.css

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -2,110 +2,43 @@ import React from "react";
 
 const Join = () => {
   return (
-    <div
-      style={{
-        textAlign: "center",
-        backgroundColor: "#000",
-        padding: "90px",
-        position: "relative",
-        background: "linear-gradient(170.23deg, #241d3b 15%, #000000 121%)",
-      }}
-    >
+    <div className="relative bg-black bg-gradient-to-b from-[#241d3b] to-black py-[90px] text-center">
       {/* Content Section */}
-      <div style={{ position: "relative", zIndex: 1 }}>
-        <h1 style={{ fontSize: "36px", fontFamily: "Nunito, sans-serif" }}>
-          Join
-        </h1>
+      <div className="relative z-10">
+        <h1 className="font-nunito text-[36px]">Join</h1>
 
-        <div style={{ position: "relative", display: "inline-block" }}>
+        <div className="relative inline-block">
           {/* Medium Frame */}
           <img
             src="/frame/joinframe1.svg"
             alt="Medium Frame"
-            style={{ width: "1569px", height: "588px" }}
+            className="h-[588px] w-[1569px]"
           />
 
           {/* Dark Gradient Overlay for Text - Contained within the small frame */}
-          <div
-            style={{
-              position: "absolute",
-              top: "-54px",
-              left: "50%",
-              transform: "translateX(-50%)",
-              width: "588px",
-              height: "125px",
-              background:
-                "linear-gradient(170.23deg, #15121A 15%, #675980 121%)",
-              opacity: 1,
-              zIndex: 0,
-              pointerEvents: "none",
-            }}
-          ></div>
+          <div className="pointer-events-none absolute left-1/2 top-[-54px] z-0 h-[125px] w-[588px] -translate-x-1/2 transform bg-gradient-to-b from-[#15121A] to-[#675980] opacity-100"></div>
 
           {/* Small Frame - Positioned on top of the Medium Frame */}
           <img
             src="/frame/smallframe.svg"
             alt="Small Frame"
-            style={{
-              width: "670.53px",
-              height: "126.8px",
-              position: "absolute",
-              top: "-55px",
-              left: "50%",
-              transform: "translateX(-50%)",
-              zIndex: 1,
-            }}
+            className="absolute left-1/2 top-[-55px] z-10 h-[126.8px] w-[670.53px] -translate-x-1/2 transform"
           />
 
           {/* Star Icon */}
           <img
             src="/frame/star.svg"
             alt="Star"
-            style={{
-              position: "absolute",
-              top: "-65px",
-              left: "50%",
-              transform: "translateX(-50%)",
-              width: "23px",
-              height: "26px",
-              zIndex: 2,
-            }}
+            className="absolute left-1/2 top-[-65px] z-20 h-[26px] w-[23px] -translate-x-1/2 transform"
           />
 
           {/* Text inside Small Frame */}
-          <div
-            style={{
-              position: "absolute",
-              top: "1%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              fontSize: "48px",
-              color: "transparent",
-              background:
-                "linear-gradient(180deg, #D09742 20.1%, #FFDA7C 53.77%, #6A4D22 93.14%)",
-              WebkitBackgroundClip: "text",
-              fontFamily: "Nunito, sans-serif",
-              zIndex: 2,
-            }}
-          >
+          <div className="absolute left-1/2 top-[1%] z-20 -translate-x-1/2 -translate-y-1/2 transform bg-gradient-to-b from-[#D09742] via-[#FFDA7C] to-[#6A4D22] bg-clip-text font-nunito text-[48px] text-transparent">
             JOIN THE GALAXY
           </div>
 
           {/* White Text Block */}
-          <div
-            style={{
-              position: "absolute",
-              top: "150px",
-              left: "50%",
-              transform: "translateX(-50%)",
-              color: "white",
-              fontSize: "20px",
-              fontFamily: "Nunito, sans-serif",
-              width: "55%",
-              lineHeight: "1.6",
-              zIndex: 1,
-            }}
-          >
+          <div className="absolute left-1/2 top-[150px] z-10 w-[55%] -translate-x-1/2 transform font-nunito text-[20px] leading-[1.6] text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -114,86 +47,51 @@ const Join = () => {
         </div>
 
         {/* Icons Section */}
-        <div
-          style={{
-            marginTop: "-160px",
-            display: "flex",
-            justifyContent: "center",
-            gap: "90px",
-          }}
-        >
+        <div className="mt-[-160px] flex justify-center gap-[90px]">
           {/* Discord Icon */}
-          <div style={{ textAlign: "center" }}>
+          <div className="text-center">
             <img
               src="/frame/discord.svg"
               alt="Discord"
-              style={{ width: "50px", height: "50px" }}
+              className="h-[50px] w-[50px]"
             />
-            <div
-              style={{
-                color: "#D0A660",
-                marginTop: "8px",
-                marginLeft: "-2px",
-                fontFamily: "Nunito, sans-serif",
-              }}
-            >
+            <div className="-ml-[2px] mt-[8px] font-nunito text-[#D0A660]">
               Discord
             </div>
           </div>
 
           {/* Instagram Icon */}
-          <div style={{ textAlign: "center" }}>
+          <div className="text-center">
             <img
               src="/frame/instagram.svg"
               alt="Instagram"
-              style={{ width: "50px", height: "50px" }}
+              className="h-[50px] w-[50px]"
             />
-            <div
-              style={{
-                color: "#D0A660",
-                marginTop: "8px",
-                marginLeft: "-9px",
-                fontFamily: "Nunito, sans-serif",
-              }}
-            >
+            <div className="-ml-[9px] mt-[8px] font-nunito text-[#D0A660]">
               Instagram
             </div>
           </div>
 
           {/* Email Icon */}
-          <div style={{ textAlign: "center" }}>
+          <div className="text-center">
             <img
               src="/frame/email.svg"
               alt="Email"
-              style={{ width: "50px", height: "50px" }}
+              className="h-[50px] w-[50px]"
             />
-            <div
-              style={{
-                color: "#D0A660",
-                marginTop: "8px",
-                marginLeft: "-1px",
-                fontFamily: "Nunito, sans-serif",
-              }}
-            >
+            <div className="-ml-[1px] mt-[8px] font-nunito text-[#D0A660]">
               Email
             </div>
           </div>
 
           {/* Highlander Icon */}
-          <div style={{ textAlign: "center" }}>
+          <div className="text-center">
             <img
               src="/frame/highlander.svg"
               alt="Highlander"
-              style={{ width: "50px", height: "50px" }}
+              className="h-[50px] w-[50px]"
             />
-            <div
-              style={{
-                color: "#D0A660",
-                marginTop: "8px",
-                marginLeft: "-10px",
-                fontFamily: "Nunito, sans-serif",
-              }}
-            >
+            <div className="-ml-[10px] mt-[8px] font-nunito text-[#D0A660]">
               Highlander
             </div>
           </div>
