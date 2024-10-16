9de6658d846513fe2975e14695fc239926a9ac9c
sunbrandon <sunabrandon@gmail.com>
2024-10-15 17:06:03 -0700

    Format

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -5,7 +5,7 @@ import swLogo from "@/public/swLOGO.svg";
 
 const Landing = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="mr-28 flex h-screen items-center">
       <div className="size-2/3 text-center">
         <h1 className="mb-4 bg-gradient-to-b from-[#DDA82A] via-sw-white to-[#905803] bg-clip-text text-4xl font-bold text-transparent md:text-6xl">
           WELCOME TO
