08653c93dcaf2b3a4ebec6177ba9c0ebbbb91967
HaruSakamoto <harusakamoto803@gmail.com>
2024-10-18 13:12:47 -0700

    Fixed title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Title = ({text}:{text:string}) => {
+const Title = ({ text }: { text: string }) => {
   return (
     <div className="bg-black p-10">
       <div className="bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text text-5xl font-bold leading-tight text-transparent">
