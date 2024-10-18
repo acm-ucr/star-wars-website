01894dccc76db4aeb14ccd33cc52911cf3bae608
Jeffrey Xia <Jeffx2016@gmail.com>
2024-10-17 22:19:34 -0700

    changed hex codes to sw-gold-100 and sw-gold-200

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,13 +6,13 @@ const About = () => {
   return (
     <div className="flex w-full flex-row gap-x-[10%]">
       <div className="flex w-auto flex-col gap-y-4 pl-[12%] pt-[9%]">
-        <p className="ml-2 bg-gradient-to-b from-[#DDA82A] via-sw-white to-[#905803] bg-clip-text font-bold text-transparent sm:text-4xl md:text-5xl lg:text-6xl">
+        <p className="ml-2 bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text font-bold text-transparent sm:text-4xl md:text-5xl lg:text-6xl">
           ABOUT
         </p>
         <Image src={swLogo} alt="swLogo" className="flex w-full max-w-64" />
       </div>
       <div className="mr-[8%] flex w-auto flex-col gap-9">
-        <p className="bg-gradient-to-b from-[#DDA82A] via-sw-white to-[#905803] bg-clip-text font-bold text-transparent sm:text-4xl md:text-5xl lg:text-6xl">
+        <p className="bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text font-bold text-transparent sm:text-4xl md:text-5xl lg:text-6xl">
           Enter Our Universe
         </p>
         <p className="md:text:xl font-white sm:text-lg lg:text-2xl">
