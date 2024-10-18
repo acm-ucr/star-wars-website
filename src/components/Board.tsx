6c6ba8e66d020a06611b6e2aa96e62ef55b1531a
DannielK <dannykm003@gmail.com>
2024-10-18 13:54:41 -0700

    Addressed Comments

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,7 +3,7 @@ import { ITEMS } from "@/data/board";
 
 const Board = () => {
   return (
-    <div className="grid grid-cols-3 gap-x-32 gap-y-44">
+    <div className="flex flex-wrap gap-10">
       {ITEMS.map((item, index) => (
         <Card key={index} topText={item.topText} bottomText={item.bottomText} />
       ))}
