b8160aafd2c56a751ce4b488de3cac423b81c49b
lowbabun <chsu115@ucr.edu>
2024-10-18 22:27:14 -0700

    redo formatting for What.tsx

--- src/components/What.tsx
+++ src/components/What.tsx
@@ -5,7 +5,7 @@ import navframe from "@/public/swStar.svg";
 const What = () => {
   return (
     <>
-      <div className="flex w-full flex-row ">
+      <div className="flex w-full flex-row">
         {" "}
         <div className="flex w-auto flex-col gap-y-4 pl-[8%] pt-[9%] text-center">
           <p className="mb-2 bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-100 bg-clip-text text-4xl font-bold text-transparent">
@@ -20,7 +20,7 @@ const What = () => {
             aliquip ex ea commodo consequat.
           </p>
 
-          <p className=" w-4/5 rounded-sm text-right font-nunito text-3xl text-white">
+          <p className="w-4/5 rounded-sm text-right font-nunito text-3xl text-white">
             Duis aute irure dolor in reprehenderit in voluptate velit esse
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
             cupidatat non proident, sunt in culpa qui officia deserunt mollit
@@ -29,11 +29,14 @@ const What = () => {
         </div>
         <div>
           <div className="sticky h-[529px] w-[535px]">
-                      <p className="absolute inset-0 flex items-center justify-center bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-100 bg-clip-text pt-[26rem] text-center text-6xl font-bold text-transparent">
+            <p className="absolute inset-0 flex items-center justify-center bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-100 bg-clip-text pt-[26rem] text-center text-6xl font-bold text-transparent">
               WHAT WE DO
             </p>
-            <Image src={navframe} alt="navframe" className=" h-full w-full object-" />
-  
+            <Image
+              src={navframe}
+              alt="navframe"
+              className="object- h-full w-full"
+            />
           </div>
         </div>
       </div>
