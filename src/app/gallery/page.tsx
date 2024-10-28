7bcab7de4a30f3205b0d5183d4d4f351d4f819e8
lowbabun <chsu115@ucr.edu>
2024-10-28 15:12:03 -0700

    update page.tsx

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,12 @@
 import Gallery from "@/components/gallery/Gallery";
+import Creation from "@/components/gallery/Creation";
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <Gallery />
+      <Creation />
     </div>
   );
 };
