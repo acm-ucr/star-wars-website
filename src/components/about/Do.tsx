be5295cf079fef43052afa2d8873da4326a0c067
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-02 15:42:38 -0700

    fix index

--- src/components/about/Do.tsx
+++ src/components/about/Do.tsx
@@ -11,7 +11,7 @@ const Do = () => {
     <div>
       <Header title="What We Do" />
       <div className="flex flex-col items-center justify-center gap-5">
-        <div className="grid grid-cols-1 gap-1 sm:grid-cols-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2">
           <div className="ml-auto max-w-sm rounded-lg border border-sw-gold-100 bg-gradient-to-r from-[#15121A] from-10% via-[#2C2636] to-[#675980] p-6 text-center shadow-md">
             <h2 className="bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text text-4xl text-transparent">
               EVENTS
