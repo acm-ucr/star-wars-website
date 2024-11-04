9ad77f1022061998a75eeb15c7002a9644a2db21
lowbabun <chsu115@ucr.edu>
2024-11-03 21:21:03 -0800

    add mobile responsive for about.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,15 +5,15 @@ import Title from "./Title";
 
 const About = () => {
   return (
-    <div className="relative flex w-full flex-row gap-x-[10%]">
+    <div className="flex w-full flex-row gap-x-[10%]">
       <div className="flex w-auto flex-col gap-y-4 pl-[12%] pt-[9%]">
         <p className="ml-2 bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text font-bold text-transparent sm:text-4xl md:text-5xl lg:text-6xl">
           ABOUT
         </p>
         <Image src={swLogo} alt="swLogo" className="flex w-full max-w-64" />
       </div>
-      <div className="mr-[8%] flex w-auto flex-col gap-9 text-left">
-        <Title text="Enter Our Galaxy" />
+      <div className="mr-[8%] flex w-auto flex-col gap-8 text-left">
+        <Title text="Enter Our Universe" />
         <p className="md:text:xl text-white sm:text-lg lg:text-2xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
