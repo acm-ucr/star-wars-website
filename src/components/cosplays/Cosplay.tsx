8c62a3cc218232310adde8aab2a9128bad00c8c8
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-17 15:18:45 -0800

    fixes

--- src/components/cosplays/Cosplay.tsx
+++ src/components/cosplays/Cosplay.tsx
@@ -1,26 +1,20 @@
 import Image from "next/image";
 import React from "react";
 import cosplaysframe from "@/public/frame/cosplaysframe.svg";
-import cardframe from "@/public/frame/cardframe.svg";
+import Card from "../Card";
+import { ITEMS } from "@/data/board";
 
 const Cosplay = ({ name, character }: { name: string; character: string }) => {
   const items = Array(6).fill(null);
 
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-10">
       <Image src={cosplaysframe} alt="cosplay frame" width={370} height={500} />
 
-      <div className="mx-5 grid grid-cols-3 gap-4">
-        {items.map((_, index) => (
-          <div key={index} className="p-4">
-            <p className="relative top-2">{name}</p>
-            <Image
-              src={cardframe}
-              alt="cosplay frame"
-              width={500}
-              height={300}
-            />
-            <p className="flex justify-end">{character}</p>
+      <div className="mx-5 flex w-full flex-wrap justify-center gap-20">
+        {ITEMS.map((item, index) => (
+          <div key={index} className="w-1/4 sm:w-1/4">
+            <Card topText={item.topText} bottomText={item.bottomText} />
           </div>
         ))}
       </div>
