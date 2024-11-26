7b27640ff6f35b51e5e02023a16095793917c7d8
Howardwheeler <howard.wheeler159@gmail.com>
2024-11-25 23:53:33 -0800

    fetch events

--- src/data/actions.ts
+++ src/data/actions.ts
@@ -1,7 +1,7 @@
 export const fetchEvents = async () => {
-  const spreadsheetID = "1T10pbPlkNok9SEFU4NAy_DGdmB_eyPDS_dhFbQ_YlDo";
+  const spreadsheetID = "empty";
   const range = "Sheet1!A1:D6";
-  const apiKey = "AIzaSyCnJLlmlCX0l7DnX0VdXEfE17Lb4Qg7u1E";
+  const apiKey = "empty";
 
   const response = await fetch(
     `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}/values/${range}?key=${apiKey}`,
