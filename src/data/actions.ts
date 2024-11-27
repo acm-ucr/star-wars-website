44ab483e08582dff2576775583550e13c7a7a41e
Howardwheeler <howard.wheeler159@gmail.com>
2024-11-27 01:37:38 -0800

    changed to calendar API

--- src/data/actions.ts
+++ src/data/actions.ts
@@ -1,8 +1,11 @@
 export const fetchEvents = async () => {
-  const date = new Date().toISOString();
+  const min = new Date().toISOString();
+  const max = new Date(
+    new Date().getTime() + 7 * 24 * 60 * 60 * 1000,
+  ).toISOString();
 
   const response = await fetch(
-    `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&timeMin=${date}&singleEvents=true&orderBy=startTime`,
+    `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_LEADS}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime&timeMin=${min}&timeMax=${max}`,
   );
   if (!response.ok) {
     throw new Error("Failed to Fetch Events");
