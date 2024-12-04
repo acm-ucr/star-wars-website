717cfce9224a00badba4bad1cfa058260c5e23be
ACM UCR <fake@domain.com>
2024-12-04 08:17:38 +0000

    convert images to webp

--- src/data/board.ts
+++ src/data/board.ts
@@ -1,9 +1,9 @@
-import Liam from "@/public/board/Liam.jpg";
-import Jose from "@/public/board/Jose.jpg";
-import Kurt from "@/public/board/Kurt.jpg";
-import Noliver from "@/public/board/Noliver.jpg";
-import Shivam from "@/public/board/Shivam.jpg";
-import Aston from "@/public/board/Aston.png";
+import Liam from "@/public/board/Liam.webp";
+import Jose from "@/public/board/Jose.webp";
+import Kurt from "@/public/board/Kurt.webp";
+import Noliver from "@/public/board/Noliver.webp";
+import Shivam from "@/public/board/Shivam.webp";
+import Aston from "@/public/board/Aston.webp";
 export const ITEMS = [
   {
     topText: "President",
