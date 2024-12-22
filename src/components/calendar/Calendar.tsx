3af5b8377d5b20b1e805c65577ae7bd45e443aff
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-12-22 01:47:34 -0800

    fixes

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -9,12 +9,7 @@ import { Loader } from "lucide-react";
 
 const CalendarCall = () => {
   const [date, setDate] = useState<Date | undefined>(new Date());
-  const {
-    data: events,
-    isFetching,
-    isError,
-    error,
-  } = useQuery({
+  const { data: events, isFetching } = useQuery({
     queryKey: ["calendarEvents"],
     queryFn: fetchCalendarEvents,
     staleTime: 1000 * 60 * 5,
@@ -22,29 +17,20 @@ const CalendarCall = () => {
   });
   const proppedEvents: EventProps[] = events || [];
 
-  if (isFetching) {
-    return (
-      <div className="flex w-full items-center justify-center gap-4 bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text text-center text-sm font-semibold text-transparent md:text-3xl">
-        Currently fetching data from hyperpace{" "}
-        <span>
-          <Loader className="text-sw-white" />
-        </span>
-      </div>
-    );
-  }
-  if (isError) {
-    return (
-      <div className="bg-red-500 bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text p-4 text-center text-sm font-semibold text-transparent md:text-3xl">
-        Error: {error.message}
-      </div>
-    );
-  }
   return (
     <div className="flex w-full flex-col items-center">
       <div className="flex w-fit items-center justify-center md:w-2/5">
         <Header title="Event Calendar" />
       </div>
       <div className="relative w-screen overflow-clip">
+        {isFetching && (
+          <div className="flex w-full items-center justify-center gap-4 bg-gradient-to-b from-sw-gold-100 via-sw-white to-sw-gold-200 bg-clip-text text-center text-sm font-semibold text-transparent md:text-3xl">
+            Currently fetching data from hyperpace{" "}
+            <span>
+              <Loader className="text-sw-white" />
+            </span>
+          </div>
+        )}
         <Calendar
           mode="single"
           selected={date}
