8a2cc13725602dedd5fd0ce5f570f116653f7414
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-05-10 13:56:09 -0700

    fixed

--- src/components/calendar/EventCard.tsx
+++ src/components/calendar/EventCard.tsx
@@ -16,7 +16,7 @@ const EventCard = ({
   return (
     <div className="m-0 flex flex-col border-2 border-white md:m-10">
       <div className="flex justify-center bg-sw-gold-100 bg-gradient-to-r from-sw-gold-100 via-sw-white to-sw-gold-200">
-        <span className="my-[5%] font-semibold text-white sm:text-base md:text-xl lg:text-3xl">
+        <span className="my-[5%] text-center font-semibold text-white sm:text-base md:text-xl lg:text-3xl">
           {name}
         </span>
       </div>
@@ -24,7 +24,7 @@ const EventCard = ({
         <Image
           src={logo}
           alt="Logo"
-          className="absolute -bottom-6 left-0 w-2/12 bg-black md:left-2 md:w-fit"
+          className="absolute -bottom-6 left-0 w-1/12 bg-black md:left-2 md:w-fit"
         />
         <div>{date}</div>
         <div>{time}</div>
